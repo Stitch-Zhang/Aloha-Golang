00e9b5dec0d9760634880257a6f6a9341a37a13a
Stitch-Zhang <Stitch-Zhang@users.noreply.github.com>
2020-02-23 21:41:56 +0800

    Transform Any Hex

---
+++ Transform Hex/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var result string
+
+func main() {
+	number := 200                        //要转换的数字
+	Hex := 3                             //转换的进制
+	result = (transformHex(number, Hex)) //将 数字和进制参数带入transformHex函数中
+	fmt.Print(result)                    //显示结果
+}
+
+func transformHex(a int, Hex int) string {
+	result := ""
+	for ; a > 0; a /= Hex {
+		last := a % Hex
+		result = strconv.Itoa(last) + result
+	}
+	return result //返回计算后结果
+}
